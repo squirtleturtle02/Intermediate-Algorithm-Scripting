41ce307192f70a5ba5a76c664653bc66b0d5cf6b
Manuel Torres <manueljosetorres02@gmail.com>
2018-01-22 23:01:53 -0600

    Some additional Challenges

---
+++ Intermediate Algorithm Screwing/BooWho/Javascript.js
@@ -0,0 +1,11 @@
+
+function booWho(bool) {
+  //type of operator will return a string indicating the type of the unevaluated operand
+  // What is the new fad diet for ghost developers? The Boolean.
+
+//check if it is a primitive boolean value
+  if(typeof booWho ==='boolean')
+  //if not return null
+}
+
+booWho(null);
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Intermediate Algorithm Scripting
+<h1> This is the list of the intermediate algorithm Javascript scripting section for Free Code Camp </h1>
+
+<ul>
+<li> <a>  Sum all Numbers in a range <a> </li>
+</ul>
